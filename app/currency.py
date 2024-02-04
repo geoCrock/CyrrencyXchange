bc39309d06b777e3e5b5d70ddaf7b769f56fac80
geoCrock <georgiy.pypok1337@gmail.com>
2024-02-05 00:12:55 +0300

    chache ready

--- app/currency.py
+++ app/currency.py
@@ -1,6 +1,5 @@
 from decimal import Decimal
 import ccxt.async_support as ccxt
-import asyncio
 import redis
 from log import logger
 
@@ -16,14 +15,14 @@ async def add_to_redis_rub(pair, exchange_id, ask_price):
     info_list = [exchange_id, ask_price]
     r.delete(pair)
     r.rpush(pair, *info_list)
-    logger.info(f'Установилось значение для redis на {pair}: {r.lrange(pair, 0, -1)}')
+    logger.info(f'Установилось значение в redis на {pair}: {r.lrange(pair, 0, -1)}')
 
 
 async def add_to_redis_usd(pair, exchange_id, ask_price):
     info_list = [exchange_id, ask_price]
     r.delete(pair)
     r.rpush(pair, *info_list)
-    logger.info(f'Установилось значение для redis на {pair}: {r.lrange(pair, 0, -1)}')
+    logger.info(f'Установилось значение в redis на {pair}: {r.lrange(pair, 0, -1)}')
 
 
 async def fetch_ticker(exchange, pair):
@@ -34,7 +33,7 @@ async def fetch_ticker(exchange, pair):
         if not ask_price == 'None':
             await add_to_redis_usd(pair, exchange.id, ask_price)
             await add_to_redis_rub(pair, exchange.id, ask_price)
-            return pair
+        return ask_price  # Возвращаем полученное значение
     except ccxt.NetworkError as e:
         logger.warning(f"Ошибка сети при обращении к {exchange.id}: {e}")
     except ccxt.ExchangeError as e:
@@ -46,31 +45,28 @@ async def fetch_ticker(exchange, pair):
 
 async def get_all_cyrrency():
     # Создаем список бирж, к которым хотим подключиться
-    # exchanges = (ccxt.kraken(), ccxt.bitfinex(),)
-    exchanges = (ccxt.kraken(),)
+    exchanges = (ccxt.kraken(), ccxt.bitfinex(), ccxt.binance(), ccxt.coinex(),
+                 ccxt.coinbase(), ccxt.huobi(), ccxt.bitstamp(), ccxt.gateio())
 
     # Определяем валютные пары, для которых хотим получить курсы
     currency_pairs = ('BTC/USD', 'ETH/USD', 'USDT/USD')
 
-    # Создаем словарь для хранения курсов
-    currency_rates = {}
+    # Создаем словарь для хранения уже полученных значений курсов
+    cached_tickers = {}
 
     # Создаем список задач для выполнения асинхронно
-    tasks = []
     for pair in currency_pairs:
         for exchange in exchanges:
-            tasks.append(fetch_ticker(exchange, pair))
-
-    # Запускаем все задачи и ждем их завершения
-    results = await asyncio.gather(*tasks)
-
-    # Заполняем словарь найденными курсами
-    # for pair, rate in zip(currency_pairs, results):
-    #     if rate is not None:
-    #         currency_rates[pair] = rate
+            # Проверяем, есть ли уже значение в кэше
+            if pair not in cached_tickers:
+                # Если нет, делаем запрос и сохраняем значение в кэше
+                cached_tickers[pair] = await fetch_ticker(exchange, pair)
+            else:
+                # Если значение уже есть в кэше, используем его
+                logger.info(f"Значение для {pair} уже получено: {cached_tickers[pair]}")
 
     # Закрываем ресурсы для каждой биржи
     for exchange in exchanges:
         await exchange.close()
 
-    return currency_rates
+    logger.info(f'Кэш: {cached_tickers}')
