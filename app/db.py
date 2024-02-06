83d97217ee70cc71f82228101b71a47e5bd8fad2
geoCrock <georgiy.pypok1337@gmail.com>
2024-02-06 16:40:35 +0300

    app

--- app/db.py
+++ app/db.py
@@ -6,13 +6,11 @@ from sqlalchemy import func
 
 from app.config import POSTGRESQL_URL
 
-# Connecting to the database
 DATABASE_URL = POSTGRESQL_URL
 database = Database(DATABASE_URL)
 metadata = declarative_base()
 
 
-# Defining a model for a table in the database
 class CurrencyTable(metadata):
     __tablename__ = "currency"
     id = Column(Integer, primary_key=True, index=True)
@@ -22,9 +20,7 @@ class CurrencyTable(metadata):
     price = Column(DECIMAL)
 
 
-# Create a table in the database
 engine = create_engine(DATABASE_URL)
 metadata.metadata.create_all(bind=engine)
 
-# Create a session to work with the database
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
